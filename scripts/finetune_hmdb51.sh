6045af036c158d08d5b67e29ee3da3f9f8a8ba63
taojiashu <henrytjsh@gmail.com>
2019-06-28 10:14:16 +0800

    Specify pre-trained model path

--- scripts/finetune_hmdb51.sh
+++ scripts/finetune_hmdb51.sh
@@ -17,7 +17,7 @@ python tools/train_net.py \
 --test_data=/home/jiashu/Documents/VMZ/data/hmdb51_test01 \
 --model_name=r2plus1d --model_depth=34 \
 --clip_length_rgb=32 --batch_size=4 \
---pretrained_model=/mnt/homedir/trandu/video_models/kinetics/l32/r2.5d_d34_l32.pkl \
+--pretrained_model=/home/jiashu/Documents/VMZ/model/r2.5d_d18_l16.pkl \
 --db_type='pickle' --is_checkpoint=0 \
 --gpus=0,1,2,3,4,5,6,7 --base_learning_rate=0.0002 \
 --epoch_size=40000 --num_epochs=8 --step_epoch=2 \
